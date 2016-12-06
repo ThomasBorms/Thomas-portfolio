37cddb36f3b35daf54d7752bf3c327790ca31e08
ThomasBorms <thomas.borms@hotmail.com>
2016-12-06 14:21:02 +0100

    clear

--- js/fast.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-    	navigation: true,
-    	anchors:['intro', 'about', 'gallery', 'contact'],
-    	verticalCentered: true
-    });
-});
-
